c55c2f552258a8e4eb1ed7cf51e364a7ad5c87b6
kyand38 <kyand2024@gmail.com>
2024-12-05 17:54:49 -0600

    Got the QuizCard component started returning api call

---
+++ client/src/components/QuizCard.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+
+
+interface QuestionCardProps {
+    question: string;
+    answers: string[];
+    correct: string;
+    category: string;
+    explanation: string;
+  }
+
+
+const QuizCard = () => {
+
+    const [trivia, setTrivia] = useState<QuestionCardProps | null>(null)
+    
+    const getRandomQuestion = async () => {
+        const url = 'https://quizmania-api.p.rapidapi.com/random-trivia';
+        const options = {
+            method: 'GET',
+            headers: {
+		'x-rapidapi-key': import.meta.env.VITE_QUIZMASTER_API_KEY,
+		'x-rapidapi-host': 'quizmania-api.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+    setTrivia(result)
+    console.log("Here is trivia: ", trivia)
+} catch (error) {
+	console.error(error);
+}
+
+
+
+    }
+return  (
+        <>
+        <div>
+                {trivia === null?(
+                    <p>Click da button!!!</p>
+                ): (
+                    <h2>Category: {trivia.category}</h2>
+                    <p></p>
+
+                )
+            }
+        </div>
+<button onClick={getRandomQuestion}>
+    Get Random Question
+</button>
+</>
+)
+}
+
+export default QuizCard
