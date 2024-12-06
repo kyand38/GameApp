998de95f92e58c95e9dc4be5c6de61eda9448572
MrBalld <balld112205@gmail.com>
2024-12-06 13:13:55 -0600

    added Navbars

--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -1,11 +1,15 @@
-
+import Navbar from "../components/navbar";
 import QuizCard from "../components/QuizCard";
 
 const Game = () => {
 
     return (
         <>
+        <h1>Triva Titans</h1>
+        <Navbar/>
+        <div>
         <QuizCard/>
+        </div>
         </>
     )
 }
