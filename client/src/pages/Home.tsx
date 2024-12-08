6877730a361af80527ec2e8bc1a53cfaf65c9825
JoshAskew <asksprit3@gmail.com>
2024-12-07 22:01:29 -0600

    made fireworks component, added conditionals and animations to the 21random game

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,6 +6,7 @@ import { useEffect } from 'react';
 import { gsap } from 'gsap';
 import Navbar from '../components/navbar';
 import Logo from '../images/trivia-titans-logo.png'
+import Fireworks from '../components/Fireworks';
 
 const useStyle = createStyles(({ prefixCls, css }) => ({
     linearGradientButton: css`
@@ -122,42 +123,6 @@ const flickerEffect = () => {
 };
 flickerEffect();
 
-// Fireworks effect function
-const Fireworks = () => {
-    const createFirework = () => {
-        for (let i = 0; i < 20; i++) {
-            const firework = document.createElement('div');
-            firework.className = 'firework';
-            document.body.appendChild(firework);
-
-            firework.style.position = 'absolute';
-            firework.style.top = `${Math.random() * window.innerHeight}px`;
-            firework.style.left = `${Math.random() * window.innerWidth}px`;
-            firework.style.width = '10px';
-            firework.style.height = '10px';
-            firework.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 60%)`;
-            firework.style.borderRadius = '50%';
-            firework.style.opacity = '1';
-
-            gsap.to(firework, {
-                x: Math.random() * 800 - 400,
-                y: Math.random() * 800 - 400,
-                scale: Math.random() * 2 + 1,
-                opacity: 0,
-                duration: 2 + Math.random() * 2,
-                ease: 'power4.out',
-                onComplete: () => firework.remove(),
-            });
-        }
-    };
-
-    useEffect(() => {
-        const interval = setInterval(createFirework, 500);
-        return () => clearInterval(interval);
-    }, []);
-
-    return null;
-};
 
 // Home Component
 const Home = () => {
